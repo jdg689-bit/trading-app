41b554c511502a008d613ab5b3ed7829e0ae8096
Jacob de Graaf <jdgraaf689@gmail.com>
2024-06-02 08:01:30 +1000

    replacing localhost fetch request urls w/ Heroku app urls

--- front-end/finance/src/components/Login/Login.jsx
+++ front-end/finance/src/components/Login/Login.jsx
@@ -6,7 +6,7 @@ async function registerUser(credentials) {
     // Create user document in db using form data
 
     try {
-        const response = await fetch('http://localhost:3000/register', {
+        const response = await fetch('https://git.heroku.com/trading-platform.git/register', {
             method : "POST",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify(credentials)
